a0f6c3f226fc7150e49eeb126ef47b407cc9ca50
TrywialnyOrzech <natan.orze@gmail.com>
2021-04-29 00:22:20 +0200

    Add program termination via SIGINT.

--- main.cc
+++ main.cc
@@ -1,15 +1,30 @@
 // C++ libs
+#include <stdlib.h>
+
 #include <iostream>
 #include <string>
 // System libs
-
+#include <signal.h>
 // Libs
 #include "src/antivirus/headers/Antivirus.h"
 #include "src/antivirus/headers/Mode.h"
 
 using namespace std;
 
+void handler(int signum);
+
 int main() {
+  sighandler_t old;
+  old = signal(SIGINT, &handler);
+  if (old == SIG_ERR) {
+    cout << "Could not handle this signal." << endl;
+    return 1;
+  }
   Antivirus antivirus;
   return 0;
+}
+
+void handler(int signum) {
+  cout << "Sent shutdown signal (" << signum << ")" << endl;
+  exit(0);
 }
